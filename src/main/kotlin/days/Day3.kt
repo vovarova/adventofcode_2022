ef4c7021d5bd73dcc5f6a8142f87069c71d81915
Volodymyr Roman <volodymyr.roman@tui.be>
2022-12-03 12:30:39 +0200

    day 3: make everything more Kotlin like, and little bit more complex

--- src/main/kotlin/days/Day3.kt
+++ src/main/kotlin/days/Day3.kt
@@ -13,25 +13,12 @@ class Day3 : Day(3) {
 
     override fun partOne(): Any {
         return inputList.map {
-            var result = 0
-            val halfSize = it.length / 2
-            val charsSet = mutableSetOf<Char>()
-            for (index in 0..halfSize - 1) {
-                charsSet.add(it[index])
-            }
-            for (index in halfSize..it.length-1) {
-                if (charsSet.contains(it[index])) {
-                    result = priority(it[index])
-                    break
-                }
-            }
-            result
-        }.sum()
+            it.toCharArray(0, it.length / 2).toSet().intersect(it.toCharArray(it.length / 2).toSet()).iterator().next()
+        }.map { priority(it) }.sum()
     }
 
-
     override fun partTwo(): Any {
-        return inputList.chunked( 3){
+        return inputList.chunked(3) {
             it[0].toSet().intersect(it[1].toSet()).intersect(it[2].toSet()).first()
         }.map { priority(it) }.sum()
     }
