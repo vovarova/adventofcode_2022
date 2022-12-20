9e52297e858377bb6f0b7ef6a79962c27068c3a8
Volodymyr Roman <volodymyr.roman@tui.be>
2022-12-21 00:50:20 +0200

    Day 18 2-st part

--- src/main/kotlin/days/Day18.kt
+++ src/main/kotlin/days/Day18.kt
@@ -1,44 +1,53 @@
 package days
 
+import java.util.*
+
 
 class Day18 : Day(18) {
 
-    data class Point(val x: Int, val y: Int, val z: Int)
-    class Cube(val point: Point) {
-        val coordinates: List<Point> = IntRange(point.x - 1, point.x).asSequence()
+    data class Point2D(val x: Int, val y: Int)
+    data class Point3D(val x: Int, val y: Int, val z: Int)
+    class Cube(val aPoint3D: Point3D) {
+        val coordinates: List<Point3D> = IntRange(aPoint3D.x - 1, aPoint3D.x).asSequence()
             .flatMap { x ->
-                IntRange(point.y - 1, point.y).asSequence().flatMap { y ->
-                    IntRange(point.z - 1, point.z).map { z ->
+                IntRange(aPoint3D.y - 1, aPoint3D.y).asSequence().flatMap { y ->
+                    IntRange(aPoint3D.z - 1, aPoint3D.z).map { z ->
 
-                        Point(x, y, z)
+                        Point3D(x, y, z)
                     }
                 }
             }.toList()
 
-        val sideX = Side(coordinates.filter { it.x == point.x }.toSet())
-        val `sideX-1` = Side(coordinates.filter { it.x == point.x - 1 }.toSet())
-        val `sideY-1` = Side(coordinates.filter { it.y == point.y - 1 }.toSet())
-        val sideY = Side(coordinates.filter { it.y == point.y }.toSet())
-        val sideZ = Side(coordinates.filter { it.z == point.z }.toSet())
-        val `sideZ-1` = Side(coordinates.filter { it.z == point.z - 1 }.toSet())
+        val sideX = Side(coordinates.filter { it.x == aPoint3D.x }.toSet())
+        val `sideX-1` = Side(coordinates.filter { it.x == aPoint3D.x - 1 }.toSet())
+        val `sideY-1` = Side(coordinates.filter { it.y == aPoint3D.y - 1 }.toSet())
+        val sideY = Side(coordinates.filter { it.y == aPoint3D.y }.toSet())
+        val sideZ = Side(coordinates.filter { it.z == aPoint3D.z }.toSet())
+        val `sideZ-1` = Side(coordinates.filter { it.z == aPoint3D.z - 1 }.toSet())
 
         val sides = setOf(sideX, `sideX-1`, sideY, `sideY-1`, sideZ, `sideZ-1`)
 
-        data class Side(val points: Set<Point>)
+        data class Side(val aPoint3DS: Set<Point3D>)
     }
 
     class Cubes(intput: List<String>) {
-        val cubesList: List<Cube> = intput.map {
+        val cubeIndex: Map<Point3D, Cube> = intput.map {
             it.split(",").let {
-                Cube(Point(it[0].toInt(), it[1].toInt(), it[2].toInt()))
+                Cube(Point3D(it[0].toInt(), it[1].toInt(), it[2].toInt()))
             }
-        }
+        }.map { it.aPoint3D to it }.toMap()
+        val maxX = cubeIndex.values.flatMap { it.coordinates }.map { it.x }.max()
+        val minX = cubeIndex.values.flatMap { it.coordinates }.map { it.x }.min()
+        val maxY = cubeIndex.values.flatMap { it.coordinates }.map { it.y }.max()
+        val minY = cubeIndex.values.flatMap { it.coordinates }.map { it.y }.min()
+        val maxZ = cubeIndex.values.flatMap { it.coordinates }.map { it.z }.max()
+        val minZ = cubeIndex.values.flatMap { it.coordinates }.map { it.z }.min()
     }
 
     override fun partOne(): Any {
         var square: Int = 0
         val cubes = Cubes(inputList)
-        val cubesList = cubes.cubesList
+        val cubesList = cubes.cubeIndex.values.toList()
         for (i in cubesList.indices) {
             square += cubesList[i].sides.size
             for (j in i + 1 until cubesList.size) {
@@ -49,6 +58,37 @@ class Day18 : Day(18) {
     }
 
     override fun partTwo(): Any {
-        return ""
+        val cubes = Cubes(inputList)
+        val visited = mutableSetOf<Point3D>()
+        val empty = mutableSetOf<Cube>()
+        val pointStack = LinkedList<Point3D>()
+        pointStack.add(Point3D(cubes.maxX + 1, cubes.maxY + 1, cubes.maxZ + 1))
+        while (pointStack.isNotEmpty()) {
+            val poll = pointStack.poll()
+            if (visited.contains(poll)) {
+                continue
+            }
+            if (poll.x > cubes.maxX + 1 || poll.x < cubes.minX - 1
+                || poll.y > cubes.maxY + 1 || poll.y < cubes.minY - 1
+                || poll.z > cubes.maxY + 1 || poll.z < cubes.minZ - 1
+            ) {
+                continue
+            }
+
+            visited.add(poll)
+            if (!cubes.cubeIndex.containsKey(poll)) {
+                val element = Cube(poll)
+                empty.add(element)
+                pointStack.add(Point3D(poll.x - 1, poll.y, poll.z))
+                pointStack.add(Point3D(poll.x + 1, poll.y, poll.z))
+                pointStack.add(Point3D(poll.x, poll.y + 1, poll.z))
+                pointStack.add(Point3D(poll.x, poll.y - 1, poll.z))
+                pointStack.add(Point3D(poll.x, poll.y, poll.z + 1))
+                pointStack.add(Point3D(poll.x, poll.y, poll.z - 1))
+            }
+        }
+        return cubes.cubeIndex.values.flatMap { it.sides }.distinct().intersect(
+            empty.flatMap { it.sides }.distinct()
+        ).size
     }
 }
