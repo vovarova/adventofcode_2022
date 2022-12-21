662585b6e949e4af295f6aca2e9ba5b6b9c1e48b
Volodymyr Roman <volodymyr.roman@tui.be>
2022-12-21 11:11:13 +0200

    Optimize Day 18

--- src/main/kotlin/days/Day18.kt
+++ src/main/kotlin/days/Day18.kt
@@ -7,35 +7,87 @@ class Day18 : Day(18) {
 
     data class Point2D(val x: Int, val y: Int)
     data class Point3D(val x: Int, val y: Int, val z: Int)
-    class Cube(val aPoint3D: Point3D) {
-        val coordinates: List<Point3D> = IntRange(aPoint3D.x - 1, aPoint3D.x).asSequence()
+    class Cube(val point3D: Point3D) {
+        val coordinates: List<Point3D> = IntRange(point3D.x - 1, point3D.x).asSequence()
             .flatMap { x ->
-                IntRange(aPoint3D.y - 1, aPoint3D.y).asSequence().flatMap { y ->
-                    IntRange(aPoint3D.z - 1, aPoint3D.z).map { z ->
+                IntRange(point3D.y - 1, point3D.y).asSequence().flatMap { y ->
+                    IntRange(point3D.z - 1, point3D.z).map { z ->
 
                         Point3D(x, y, z)
                     }
                 }
             }.toList()
 
-        val sideX = Side(coordinates.filter { it.x == aPoint3D.x }.toSet())
-        val `sideX-1` = Side(coordinates.filter { it.x == aPoint3D.x - 1 }.toSet())
-        val `sideY-1` = Side(coordinates.filter { it.y == aPoint3D.y - 1 }.toSet())
-        val sideY = Side(coordinates.filter { it.y == aPoint3D.y }.toSet())
-        val sideZ = Side(coordinates.filter { it.z == aPoint3D.z }.toSet())
-        val `sideZ-1` = Side(coordinates.filter { it.z == aPoint3D.z - 1 }.toSet())
+
+        val sideX = SideV2(
+            maxX = point3D.x,
+            minX = point3D.x,
+            maxY = point3D.y,
+            minY = point3D.y - 1,
+            maxZ = point3D.z,
+            minZ = point3D.z - 1
+        )
+        val `sideX-1` = SideV2(
+            maxX = point3D.x - 1,
+            minX = point3D.x - 1,
+            maxY = point3D.y,
+            minY = point3D.y - 1,
+            maxZ = point3D.z,
+            minZ = point3D.z - 1
+        )
+        val `sideY` = SideV2(
+            maxX = point3D.x,
+            minX = point3D.x - 1,
+            maxY = point3D.y,
+            minY = point3D.y,
+            maxZ = point3D.z,
+            minZ = point3D.z - 1
+        )
+        val `sideY-1` = SideV2(
+            maxX = point3D.x,
+            minX = point3D.x - 1,
+            maxY = point3D.y - 1,
+            minY = point3D.y - 1,
+            maxZ = point3D.z,
+            minZ = point3D.z - 1
+        )
+
+        val `sideZ` = SideV2(
+            maxX = point3D.x,
+            minX = point3D.x - 1,
+            maxY = point3D.y,
+            minY = point3D.y - 1,
+            maxZ = point3D.z,
+            minZ = point3D.z
+        )
+        val `sideZ-1` = SideV2(
+            maxX = point3D.x,
+            minX = point3D.x - 1,
+            maxY = point3D.y,
+            minY = point3D.y - 1,
+            maxZ = point3D.z - 1,
+            minZ = point3D.z - 1
+        )
 
         val sides = setOf(sideX, `sideX-1`, sideY, `sideY-1`, sideZ, `sideZ-1`)
 
-        data class Side(val aPoint3DS: Set<Point3D>)
+        data class SideV2(
+            val maxX: Int,
+            val minX: Int,
+            val maxY: Int,
+            val minY: Int,
+            val maxZ: Int,
+            val minZ: Int
+        )
     }
+
 
     class Cubes(intput: List<String>) {
         val cubeIndex: Map<Point3D, Cube> = intput.map {
             it.split(",").let {
                 Cube(Point3D(it[0].toInt(), it[1].toInt(), it[2].toInt()))
             }
-        }.map { it.aPoint3D to it }.toMap()
+        }.map { it.point3D to it }.toMap()
         val maxX = cubeIndex.values.flatMap { it.coordinates }.map { it.x }.max()
         val minX = cubeIndex.values.flatMap { it.coordinates }.map { it.x }.min()
         val maxY = cubeIndex.values.flatMap { it.coordinates }.map { it.y }.max()
@@ -61,6 +113,7 @@ class Day18 : Day(18) {
         val cubes = Cubes(inputList)
         val visited = mutableSetOf<Point3D>()
         val empty = mutableSetOf<Cube>()
+        val cubesToCompare = mutableSetOf<Cube>()
         val pointStack = LinkedList<Point3D>()
         pointStack.add(Point3D(cubes.maxX + 1, cubes.maxY + 1, cubes.maxZ + 1))
         while (pointStack.isNotEmpty()) {
@@ -77,8 +130,8 @@ class Day18 : Day(18) {
 
             visited.add(poll)
             if (!cubes.cubeIndex.containsKey(poll)) {
-                val element = Cube(poll)
-                empty.add(element)
+                val currentCube = Cube(poll)
+                empty.add(currentCube)
                 pointStack.add(Point3D(poll.x - 1, poll.y, poll.z))
                 pointStack.add(Point3D(poll.x + 1, poll.y, poll.z))
                 pointStack.add(Point3D(poll.x, poll.y + 1, poll.z))
@@ -91,4 +144,8 @@ class Day18 : Day(18) {
             empty.flatMap { it.sides }.distinct()
         ).size
     }
+}
+
+fun main() {
+    println(Day18().partTwo())
 }
